cb0833ebad3f188ff658cf7967fa33d9cfb69744
Carlos Gabriel Medeiros da Silva <90879208+CarlosG18@users.noreply.github.com>
2025-05-22 12:04:30 -0300

    Update script.py

--- uni1/atividade3/script.py
+++ uni1/atividade3/script.py
@@ -6,8 +6,26 @@ spark = (
         .master("yarn")
         .appName("People Count")
         .getOrCreate()
-        .sparkContext
 )
 
 # definindo o contexto
-rdd = spark.textFile("myfiles/conjunto2.csv")
+rdd = spark.sparkContext.textFile("file:///home/myuser/myfiles/README.md")
+
+rdd = spark.sparkContext.textFile("file:///home/myuser/myfiles/conjunto2.csv")
+
+#removendo o cabeçalho
+head = rdd.first()
+dados = rdd.filter(lambda linha: linha != head)
+
+# Separando os campos por vírgula e tratando os tipos
+dados_processados = dados.map(lambda linha: linha.split(",")) \
+                         .map(lambda campos: (campos[0],campos[1]))  # pegando o nome do cargo
+
+#retirando as duplicatas
+remove_duplicatas = dados.distinct()
+
+remove_duplicatas.collect()
+
+#dados_processados.collect()
+
+dados_processados.distinct().collect()
